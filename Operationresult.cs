aaf59030c8630fad8aaf9ff4f16379b57f286c2f
OchiengHosea <jrhosea8@gmail.com>
2022-01-13 22:02:34 +0300

    Generic types definition

--- Operationresult.cs
+++ Operationresult.cs
@@ -1,11 +1,14 @@
 using System;
 
 namespace ACM{
-    public class OperationResult
+    public class OperationResult<T>
     {
-        public OperationResult()
+        public T Result { get; set; }
+        public string Message { get; set; }
+        public OperationResult(T result, string message)
         {
-            
+            this.Result = result;
+            this.Message = message;
         }
     }
 }
